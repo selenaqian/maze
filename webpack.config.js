7d9eb741bfa562d6b08b4e0f9d6512bcb7ab48db
Selena Qian <sqian@pbs.org>
2022-11-17 11:54:59 -0500

    Development environment setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env) => {
+  return {
+    mode: env.dev ? 'development' : 'production',
+    entry: {
+      index: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      https: true,
+      static: './dist',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Develop',
+      }),
+    ],
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+    },
+  }  
+};
